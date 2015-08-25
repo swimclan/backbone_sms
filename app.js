4e29e9d9d2a58164a6bd4ee7a55c4141c93fa3e9
Matthew Herron <matthew.herron77@gmail.com>
2015-08-25 15:03:53 -0500

    Its a final version of the simple SMS backbone app

--- app.js
+++ app.js
@@ -26,25 +26,42 @@ sms.collectionView = Backbone.View.extend({
 	},
 	initialize: function() {
 		console.log('A new collection view has been instantiated');
+		var that = this;
+		this.collection.on('change', function() {
+			
+		});
 	},
 	render: function() {
+		console.log('fucking shit..');
+		var collection = this.collection.models;
+		console.log(collection.length);
+		for (var model in collection) {
+			new sms.modelView({
+				model: collection[model]
+			});
+		}
 		console.log('you clicked the button..');
-
 	}
 });
 
 sms.modelView = Backbone.View.extend({
+	el: $('.list'),
 	initialize: function() {
 		console.log('A new model view has been instantiated');
+		this.render();
 	},
 	render: function() {
-
+		var data = this.model.attributes;
+		var tpl = "Sent from: " + data.sender + " >> " + data.message;
+		this.$el.append(tpl);
 	}
 });
 
 /*-------------------------------------
-
+Helper functions
 -------------------------------------*/
+
+
 
 /*-------------------------------------
 Main program, events and triggers
@@ -55,7 +72,12 @@ sms.active.collectionView = new sms.collectionView({
 	el: $('.ui'),
 	collection: sms.active.collection
 });
-
+$('#new').on('click', function() {
+	sms.active.collection.create({
+		sender: 'Lesley Grill',
+		message: 'Can you pick up dog food???'
+	})
+});
 
 
 }); // end document.ready
